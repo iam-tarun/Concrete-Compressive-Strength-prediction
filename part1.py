8c019f782711f5fe7472c0669d6cb1e53cfbec85
Tarunteja Obbina <tarun@Taruntejas-MacBook-Pro.local>
2023-09-17 21:59:54 -0500

    add comments

--- part1.py
+++ part1.py
@@ -5,7 +5,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import norm
 
-
+"""
+  Global Config dict to set hyper params and other required booleans
+  
+"""
 config = {
   'dataset': 'https://drive.google.com/uc?id=1kCozLy5zNmPkeC6E47cQziPpjWpm5XZL',
   'train-fraction': 0.8, 
@@ -16,45 +19,49 @@ config = {
   'actual-value': 'Concrete-compressive-strength',
   'convergence-threshold': 0.0005,
   'display-plots': False,
+  'plotFilePath': './part1-logfile.csv'
 }
 
-
-
+"""
+  LinearRegression class to train and predict the concrete compressive strength
+"""
 class LinearRegression:
   def __init__(self, dataset: str, actual_value: str):
-    self.dataset = dataset
-    self.actual_value = actual_value
-    self.data = None
-    self.scaled_data = None
-    self.train_data = None
-    self.test_data = None
-    self.weights = None
-    self.features_len = None
-    self.train_x = None
-    self.train_Y = None
-    self.test_x = None
-    self.test_Y = None
-    self.log = []
-    self.feature_stats = {}
-    self.displayPlots = None
+    self.dataset = dataset # path to the dataset
+    self.actual_value = actual_value # actual ground truth
+    self.data = None # property to store the data frame
+    self.scaled_data = None # stores the data after it is normalized
+    self.train_data = None # stores the train data
+    self.test_data = None # stores the test data
+    self.weights = None # stores the weights (Theta values)
+    self.features_len = None # store the number of weights ( dim len of the hypothesis )
+    self.train_x = None # stores the train data of features
+    self.train_Y = None # stores the actual values of train data
+    self.test_x = None # stores the test data of features
+    self.test_Y = None # stores the actual values of test data
+    self.log = [] # list to store all kinds of stats while training ( coefficients, loss stats )
+    self.feature_stats = {} # stores the min and max value of train data for each feature
+    self.displayPlots = None # boolean to display plots
   
+  """
+    method to load the data set from given path
+  """
   def load_data(self):
     self.data = pd.read_csv(self.dataset)
     self.features_len = self.data.shape[1]
     if self.displayPlots:
+      # scatter plots between individual features and actual values
       fig, axes = plt.subplots(3, 3, figsize=(8, 8))
       for i in range(3):
         for j in range(3):
-            # Compute the index for the current subplot
             index = i * 3 + j
-            # Create a scatter plot in the current subplot
             axes[i, j].scatter(self.data.iloc[:,index], self.data.loc[:, self.actual_value], label=f'Subplot {index}')
             axes[i, j].set_title(f'Subplot {self.data.columns[index]}')
             axes[i, j].legend()
 
       plt.tight_layout()
       plt.show()
-
+  
       for col in self.data.columns:
         plt.figure(figsize=(8,8))
         ax=sns.distplot(self.data[col], fit=norm)
@@ -62,10 +69,10 @@ class LinearRegression:
         ax.axvline(self.data[col].median(), color='teal', linestyle='dashed', linewidth=2)
         ax.set_title(f'skewness of {col} : {self.data[col].skew()}')
         plt.show()
-
-    
-    
-  
+ 
+  """
+  method to remove the duplicate values in the dataset
+  """
   def filter_duplicates(self):
     duplicates = self.data.duplicated()
     if len(duplicates):
@@ -77,6 +84,9 @@ class LinearRegression:
     else:
       print("no duplicates found")
 
+  """
+  method to check the correlation between the independent features to check and remove columns which are highly correlated.
+  """
   def feature_selection(self, threshold):
     correlation_matrix = (self.data.drop(columns=[self.actual_value])).corr()
     print("correlation matrix within the features:\n")
@@ -117,6 +127,9 @@ class LinearRegression:
       sns.heatmap(correlation_matrix, vmin=-1, cmap='coolwarm', annot=True)
       plt.show()
 
+  """
+  method to normalize the features i.e, transform them into same scale
+  """
   def normalize_features(self):
     # using min max scaling
     self.scaled_data = self.data
@@ -132,13 +145,10 @@ class LinearRegression:
     print('normalized features')
     return
 
+  """
+  method to preprocess the data
+  """
   def preprocess_data(self, correlation_threshold):
-    # checking for null values
-    if self.data.isnull().sum().sum():
-      print('Null values are present')
-    else:
-      print('No null values are present')
-    
     # filtering duplicate rows
     self.filter_duplicates()
 
@@ -147,39 +157,67 @@ class LinearRegression:
 
     # normalizing the filtered features
     self.normalize_features()
-    
+
+  """
+  method to split the data frame into training set and testing set
+  """
   def split_data(self, split: float):
     self.train_data = self.scaled_data.sample(frac=split)
     self.test_data = self.scaled_data.drop(self.train_data.index)
 
+  """
+  method to split the training data frame into features and actual truth
+  """
   def train_split(self):
     self.train_x = self.train_data.drop(columns=[self.actual_value], axis=1)
+    # adding new column for intercept
     self.train_x.insert(0, 'constant', 1)
     self.train_Y = np.array(self.train_data[self.actual_value]).reshape(self.train_x.shape[0], 1)
 
+  """
+  method to split the testing data frame into features and actual truth
+  """
   def test_split(self):
     self.test_x = self.test_data.drop(columns=[self.actual_value], axis=1)
     self.test_x.insert(0, 'constant', 1)
     self.test_Y = np.array(self.test_data[self.actual_value]).reshape(self.test_x.shape[0], 1)
 
+  """
+  method to initialize the weights with random values between 0 and 1
+  """
   def initialize_weights(self):
     self.weights = np.random.rand(self.features_len, 1)
 
+  """
+  method to determine the r square score
+  """
   def R_squared_stat(self, actual, pred):
     mean_y = actual.mean()
-    RSS = ((actual - pred)**2).sum()
-    TSS = ((actual - mean_y)**2).sum()
+    RSS = ((actual - pred)**2).sum() # residual sum of squares
+    TSS = ((actual - mean_y)**2).sum() # total sum of squares
     return 1 - (RSS/TSS)
     
+  """
+  method to determine mean square error
+  """
   def MSE(self, actual, pred):
     return (((actual - pred)**2).sum())/actual.shape[0]
-
+  
+  """
+  method to determine root of mean square error
+  """
   def RMSE(self, mse):
     return np.sqrt(mse)
 
+  """
+  method to determine mean square error for testing data
+  """
   def testLoss(self):
     return self.MSE(self.test_Y, self.test_x.dot(self.weights))
 
+  """
+  method to write the log data in a csv file
+  """
   def logData(self):
     fields = ['epoch', 'trainingLoss', 'testLoss', 'epochs', 'lr', 'training_r2', 'testing_r2', 'training_rmse', 'testing_rmse']
     fields = fields + ['W'+str(i) for i in range(self.features_len)]
@@ -190,20 +228,28 @@ class LinearRegression:
     writer.writerows(self.log)
     logFile.close()
 
+  """
+  gradient descent method for updating the weights for every epoch
+  """
   def gradient_descent(self, train_T, lr, conv_threshold):
     pred_output = self.train_x.dot(self.weights)
     difference = (lr/self.train_x.shape[0])*(train_T.dot(pred_output - self.train_Y))
+    # checking if convergence threshold is reached
     if np.max(np.abs(difference)) > conv_threshold:
       self.weights -= difference
       return { 'converged': False, 'out': pred_output }
     else:
       return { 'converged': True, 'out': pred_output }
-
+  
+  """
+  method to train the data using gradient descent and logging the metrics
+  """
   def train(self, epochs: int, lr: float, conv_threshold: float):
     train_trans = np.array(self.train_x).T
     for epoch in range(epochs):
       result = self.gradient_descent(train_trans, lr, conv_threshold)
       if not result['converged']:
+        # logging the metrics into logData property of the class.
         trainingLoss = self.MSE(self.train_Y, result['out'])
         testLoss = self.testLoss()
         training_r2 = self.R_squared_stat(self.train_Y, result['out'])
@@ -235,6 +281,9 @@ class LinearRegression:
       sns.regplot(x = self.train_Y, y = self.train_x.dot(self.weights), color='green').set_title('Regression line with train data')
       plt.show()
   
+  """
+  method to predict the concrete-compressive-strength using trained model
+  """
   def predict(self, features):
     input = [1]
     for key in features.keys():
@@ -243,7 +292,10 @@ class LinearRegression:
     print(input)
     return input.dot(self.weights)
 
-  def fit(self, split, epochs, lr, threshold, conv_threshold, logging:bool = False, displayPlots: bool = False):
+  """
+  method to load and train the linear regression model
+  """
+  def fit(self, split, epochs, lr, threshold, conv_threshold, logging:bool = False, displayPlots: bool = False, plotFile: str = './part1-logfile.csv'):
     self.displayPlots = displayPlots
     self.load_data()
     self.preprocess_data(threshold)
@@ -255,52 +307,42 @@ class LinearRegression:
     if logging:
       self.logData()
     if self.displayPlots and logging:
-      logDf = pd.read_csv('./part1-logfile.csv')
-      # 'MSE plot Training'
-      plt.plot(logDf['epoch'], logDf['trainingLoss'])
-      plt.title('MSE training data vs epoch')
-      plt.show()
-      # 'MSE plot Testing'
-      plt.plot(logDf['epoch'], logDf['testLoss'])
-      plt.title('MSE testing data vs epoch')
-      plt.show()
-      # 'R2 plot Training'
-      plt.plot(logDf['epoch'], logDf['training_r2'])
-      plt.title('Training R2 score vs epoch')
-      plt.show()
-      # 'R2 plot Testing'
-      plt.plot(logDf['epoch'], logDf['testing_r2'])
-      plt.title('Testing R2 score vs epoch')
-      plt.show()
-      # 'RMSE plot Training'
-      plt.plot(logDf['epoch'], logDf['training_rmse'])
-      plt.title('Training RMSE vs epoch')
-      plt.show()
-      # 'RMSE plot Testing'
-      plt.plot(logDf['epoch'], logDf['testing_rmse'])
-      plt.title('Testing RMSE vs epoch')
-      plt.show()
-      for i in range(self.features_len):
-        plt.plot(logDf['epoch'], logDf['W'+str(i)])
-        plt.title(f'W{i} vs epoch')
-        plt.show()
-      
+      self.plotMetrics(plotFile)
 
+  """
+  method to plot the metrics data stored in the log file
+  """
+  def plotMetrics(self, file: str):
+    logDf = pd.read_csv(file)
+    # 'MSE plot Training'
+    plt.plot(logDf['epoch'], logDf['trainingLoss'])
+    plt.title('MSE training data vs epoch')
+    plt.show()
+    # 'MSE plot Testing'
+    plt.plot(logDf['epoch'], logDf['testLoss'])
+    plt.title('MSE testing data vs epoch')
+    plt.show()
+    # 'R2 plot Training'
+    plt.plot(logDf['epoch'], logDf['training_r2'])
+    plt.title('Training R2 score vs epoch')
+    plt.show()
+    # 'R2 plot Testing'
+    plt.plot(logDf['epoch'], logDf['testing_r2'])
+    plt.title('Testing R2 score vs epoch')
+    plt.show()
+    # 'RMSE plot Training'
+    plt.plot(logDf['epoch'], logDf['training_rmse'])
+    plt.title('Training RMSE vs epoch')
+    plt.show()
+    # 'RMSE plot Testing'
+    plt.plot(logDf['epoch'], logDf['testing_rmse'])
+    plt.title('Testing RMSE vs epoch')
+    plt.show()
+    # Plots for weights
+    for i in range(self.features_len):
+      plt.plot(logDf['epoch'], logDf['W'+str(i)])
+      plt.title(f'W{i} vs epoch')
+      plt.show()
 
 model = LinearRegression(config['dataset'], config['actual-value'])
-model.fit(config['train-fraction'], config['epochs'], config['lr'], config['correlation-threshold'],config['convergence-threshold'], config['logging'], config['display-plots'])
-# 139.6 ,209.4 ,0.0 ,192.0 ,0.0 ,1047.0 ,806.9 ,28 ,28.24 
-
-result = model.predict({
-  'Cement': 139.6,
-  'Blast Furnace Slag': 209.4,
-  'Fly Ash': 0.0,
-  'Water': 192.0,
-  'Superplasticizer': 0.0,
-  'Coarse Aggregate':1047.0,
-  'Fine Aggregate': 806.9,
-  'Age': 28,
-  # 'Concrete-compressive-strength': 28.24
-})
-
-print(result)
+model.fit(config['train-fraction'], config['epochs'], config['lr'], config['correlation-threshold'],config['convergence-threshold'], config['logging'], config['display-plots'], config['plotFilePath'])
